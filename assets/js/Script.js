2b6ac93b6cd3ff78a921919b9d27283a5e043fa6
abbassyyed <aabbassyyed@gmail.com>
2025-07-06 22:53:01 +0500

    Create Script.js

---
+++ assets/js/Script.js
@@ -0,0 +1,7 @@
+function handleClick(service) {
+  if (service === 'it') {
+    window.location.href = 'it-services.html';
+  } else if (service === 'trading') {
+    window.location.href = 'general-trading.html';
+  }
+}
